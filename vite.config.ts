864d065529f0fef144cdd8ea4ef9868f7886a6a0
ryannarnoldd <ryannarnoldd@gmail.com>
2024-11-15 20:19:31 -0500

    Fixing port

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   envDir: './environment',
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 3000
+  }
 });
